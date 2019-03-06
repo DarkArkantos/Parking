11a904a497e3db4aa51587926a2a71141b9c4c72
Rattenfengar <37422018+Rattenfengar@users.noreply.github.com>
2019-03-06 18:56:01 -0500

    Server Set Up

--- Server/CloudServer/CloudServer/Startup.cs
+++ Server/CloudServer/CloudServer/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace ServerParking
+namespace CloudServer
 {
     public class Startup
     {
