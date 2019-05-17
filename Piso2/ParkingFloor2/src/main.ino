0810a9aae9bae637617edd8234c1e199bafe6cfc
JHONAURBANOD <jhona.urbanod@utadeo.edu.co>
2019-05-16 20:44:26 -0500

    sdadasd

--- Piso2/ParkingFloor2/src/main.ino
+++ Piso2/ParkingFloor2/src/main.ino
@@ -4,41 +4,29 @@
 #include <ArduinoJson.h>
 #include <Carro.h>
 
-const char *ssid = "MPS";
-const char *password = "Siemenss71500";
+const char *ssid = "LAB-AUTOMATIZACION";
+const char *password = "sala.ceri";
 bool state = false;
 String send;
-WiFiServer server(80);
 Carro cars[8];
 String header;
+HTTPClient http;
+WiFiClient client;
 int x;
 void setup()
 {
   Serial.begin(9600);
   Serial.print("Conectando.. ");
   Serial.println(ssid);
+  delay(10);
   WiFi.begin(ssid, password);
-  cars[0] = Carro("AJK1", 1, 1, "Sutanito", 2, false);
-  cars[1] = Carro("AJK1", 2, 2, "Sutanito", 2, false);
-  cars[2] = Carro("AJK1", 3, 3, "Sutanito", 2, false);
-  cars[3] = Carro("AJK1", 4, 4, "Sutanito", 2, false);
-  cars[4] = Carro("AJK1", 5, 5, "Sutanito", 2, false);
-  cars[5] = Carro("AJK1", 6, 6, "Sutanito", 2, false);
-  cars[6] = Carro("AJK1", 7, 7, "Sutanito", 2, false);
-  cars[7] = Carro("AJK1", 8, 8, "Sutanito", 2, false);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-    //String licensePlate, int placeID, int carID, String owner, int floorID, bool state
-
-  }
+  while (WiFi.status() != WL_CONNECTED) 
+      delay(500);
   // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi conectado...");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  server.begin();
 
   pinMode(0, INPUT);
   pinMode(1, INPUT);
@@ -48,6 +36,14 @@ void setup()
   pinMode(5, INPUT);
   pinMode(6, INPUT);
   pinMode(7, INPUT);
+  cars[0] = Carro("AJK1", 1, 1, "Sutanito", 2, false);
+  cars[1] = Carro("AJK1", 2, 2, "Sutanito", 2, false);
+  cars[2] = Carro("AJK1", 3, 3, "Sutanito", 2, false);
+  cars[3] = Carro("AJK1", 4, 4, "Sutanito", 2, false);
+  cars[4] = Carro("AJK1", 5, 5, "Sutanito", 2, false);
+  cars[5] = Carro("AJK1", 6, 6, "Sutanito", 2, false);
+  cars[6] = Carro("AJK1", 7, 7, "Sutanito", 2, false);
+  cars[7] = Carro("AJK1", 8, 8, "Sutanito", 2, false);
 }
 void loop()
 {
@@ -73,9 +69,6 @@ void loop()
 
   if (WiFi.status() == WL_CONNECTED)
   {
-
-    HTTPClient http;
-
     http.begin("http:parkingutadeo.azuewebsites.net/api/Send");
     http.addHeader("Content-Type", "text/application/json");
 
